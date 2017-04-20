5a70461f84db1dcc9c6e3b4098692e0afae54042
RomaSlobodeniuk <romsl1991@gmail.com>
2017-04-20 23:37:28 +0300

    I've done 4-9 exercises of lesson_6.2

--- lesson_6.2/home-work-6.2.js
+++ lesson_6.2/home-work-6.2.js
@@ -55,3 +55,202 @@
 // }
 // includes('hello world', 'wor');
 //----------3. includes---------------//
+
+
+//----------4. endsWith---------------//
+//Напишите функцию endsWith(base, str), возвращает true, если строка base заканчивается подстрокой str.
+// (фичи ES6 использовать нельзя)
+
+//Solving:
+// function endsWith(base, str){
+//     var baseLength = base.length;
+//     var strLength = str.length;
+//     if (baseLength >= strLength){
+//         var number = baseLength - strLength;
+//         var check = false;
+//         for (var i = number, j = 0; i < baseLength; i++, j++){
+//             if (base[i] === str[j]){
+//                 check = true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//         if(check){
+//             return check;
+//         }
+//     }
+// }
+// var baseString = 'returning';
+// var substring = 'ning';
+// console.log('Base string = "' + baseString + '"; substring = "' + substring + '"');
+// console.log('Result:');
+// console.log(endsWith(baseString, substring));
+//----------4. endsWith---------------//
+
+
+//----------5. startsWith---------------//
+// Напишите функцию startsWith(base, str), возвращает true, если строка base начинается со строки str.
+// (фичи ES6 использовать нельзя)
+
+//Solving:
+// function startsWith(base, str) {
+//     var baseLength = base.length;
+//     var strLength = str.length;
+//     if (baseLength >= strLength){
+//         var check = false;
+//         for (var i = 0, j = 0; i < strLength; i++, j++){
+//             if (base[i] === str[j]){
+//                 check = true;
+//             } else {
+//                 return false;
+//             }
+//         }
+//         if(check){
+//             return check;
+//         }
+//     }
+// }
+// var baseString = 'returning';
+// var substring = 'retur';
+// console.log('Base string = "' + baseString + '"; substring = "' + substring + '"');
+// console.log('Result:');
+// console.log(startsWith(baseString, substring));
+//----------5. startsWith---------------//
+
+
+//----------6. repeatString---------------//
+// Напишите функцию repeat(str, times), возвращает результат повторения строки str times раз.
+// Внутри функции делать проверку аргумента : [times is целое число; times is Number; times > 0;
+// times != NaN; times != Infinity]. Если times == 0 вернуть пустую строку.
+// (фичи ES6 использовать нельзя)
+
+
+//Solving:
+// function isInteger(number) {
+//     var a = '' + number;
+//     var check = true;
+//     for(var i = 0; i < a.length; i++){
+//         if (a[i] === '.'){
+//             check = false;
+//         }
+//     }
+//     return check;
+// }
+//
+// function repeat(str, times) {
+//     if(isInteger(times) && typeof times === 'number' && times > 0 && times !== NaN && times !== Infinity ){
+//         var string = '';
+//         for(var b = 0; b < times; b++){
+//             string += str;
+//         }
+//         return string;
+//     } else if(times === 0){
+//         return '';
+//     }
+// }
+//
+// console.log(repeat('String', 9));
+//----------6. repeatString---------------//
+
+
+//----------7. reversit---------------//
+// Напишите функцию reversit(str), которая переворачивает строку str. Используйте цикл for,
+// который меняет местами первый и последний символы, затем следующие и т. д. до предпоследнего.
+// Строка str должна передаваться в функцию reversit(str) как аргумент.
+// Также организовать безопасность типов - проверять есть ли аргумент str строкой и преобразовать str в строку явно.
+
+//Solving:
+// function reversit(str) {
+//     var toString = '' + str;
+//     if(typeof toString === 'string' ){
+//         var splittedStr = toString.split('');
+//         for( var i = 0, j = -1; i < Math.round(toString.length / 2); i++){
+//             if(i === 0){
+//                 splittedStr[i] = toString.slice(-(i+1));
+//             } else {
+//                 splittedStr[i] = toString.slice(-(i+1), j);
+//                 j--;
+//             }
+//             splittedStr[splittedStr.length - i - 1] = toString[i];
+//         }
+//         var newString = '';
+//         splittedStr.forEach(function (element) {
+//             newString += element;
+//         });
+//         return newString;
+//     }
+// }
+// var inputStr = 'The long long string here';
+// console.log('Input string = "' + inputStr + '", output string = "' + reversit(inputStr) + '"');
+//----------7. reversit---------------//
+
+
+//----------8. getCharJavaScriptUltra+100500DoubleVersion---------------//
+// Напишите функцию require(n) предназначенную для получения денег данных от пользователя.
+// Функция получает аргумент n - количество символов, которые нужно получить от пользователя.
+// Использовать цикл и prompt, каждый раз запрашивать количество символов которое "должен" пользователь.
+
+//Solving:
+// var number = prompt('Enter the number of symbols:', '');
+// require(number);
+// function require(n) {
+//     var string = prompt('Enter the string according to number:', '');
+//     // console.log(string);
+//     while (string.length !== parseInt(n)){
+//        string = prompt('Stupid boy, enter the correct number of symbols!', '');
+//     }
+//     return console.log('Good boy, you have entered the right number of symbols, your string is = "' + string + '", its length = "' + n + '"');
+// }
+//----------8. getCharJavaScriptUltra+100500DoubleVersion---------------//
+
+
+//----------9. randomStr---------------//
+// Напишите функцию randomStr(n), n - длина строки из случайно сгенерированных символов.
+
+//Solving:
+// function getRandomArbitrary(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+//
+// function randomStr(n) {
+//     var str = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoP pQqRrSsTtUuVvWwXxYyZz0123456789';
+//     var randStr = '';
+//     for (var i = 0; i < n; i++){
+//         randStr += str[getRandomArbitrary(0, (str.length - 1))];
+//     }
+//     return randStr;
+// }
+// console.log(randomStr(32));
+//----------9. randomStr---------------//
+
+
+//----------10. sliceKingdom (с анг. slices - скибочки:)---------------//
+// Создайте строку randomStr(100). Разбейте эту строку на отдельные слова случайной длины и выведите в консоль.
+
+//Solving:
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomStr(n) {
+    var str = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoP pQqRrSsTtUuVvWwXxYyZz0123456789';
+    var randStr = '';
+    for (var i = 0; i < n; i++){
+        randStr += str[getRandomArbitrary(0, (str.length - 1))];
+    }
+    return randStr;
+}
+
+var randString100 = randomStr(100);
+var array = [];
+for (var b = 0; b < randString100.length; b++){
+    // array.push(randString100.slice())
+}
+
+console.log(randomStr(100));
+//----------10. sliceKingdom (с анг. slices - скибочки:)---------------//
+
+
+
+
+
